1524697109808657d9b20c5664d43890ea9a3b95
julesGoullee <julesgoullee@gmail.com>
2022-07-12 00:26:16 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('ok')
+window.addEventListener('load', Main)
+
+function Main() {
+  document.querySelectorAll('.accordion-toggle').forEach((button) => {
+    button.addEventListener('click', () => {
+      const accordion = button.nextElementSibling as HTMLElement
+      accordion.classList.toggle('show')
+      button.classList.toggle('extend')
+    })
+  })
+}
