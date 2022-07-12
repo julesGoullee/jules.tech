acf51571e25a6e9b4f3f18112aa1e1273744fcb1
julesGoullee <julesgoullee@gmail.com>
2022-07-12 12:52:43 +0200

    animation

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ window.addEventListener('load', Main)
 function Main() {
   document.querySelectorAll('.accordion-toggle').forEach((button) => {
     button.addEventListener('click', () => {
-      const accordion = button.nextElementSibling as HTMLElement
-      accordion.classList.toggle('show')
-      button.classList.toggle('extend')
+      const parent = button.parentElement as HTMLElement
+      if (
+        !parent.classList.contains('show') &&
+        !parent.classList.contains('hide')
+      ) {
+        parent.classList.add('show')
+      } else {
+        parent.classList.toggle('show')
+        parent.classList.toggle('hide')
+      }
     })
   })
 }
